5c5758efdf80a93e5e522aa88d8e47da61464278
Rubén A <sortexjuegos@gmail.com>
2024-05-27 14:06:04 -0400

    feat(wm): hyprland and gtk configs

---
+++ .config/hypr/reconnect.fish
@@ -0,0 +1,9 @@
+#! /usr/bin/fish
+
+while true
+    if not nmcli device wifi list | grep "\*" >/dev/null
+        nmcli device wifi connect 'CRISTO VIVE.'
+        dunstify "Wifi reconnected" "Reconnected to 'CRISTO VIVE.'"
+    end
+    sleep 2
+end
